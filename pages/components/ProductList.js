0dffa0e551b3c035f005468fba99ecbfa040fe6a
hotmailbelike <mdabdullahomar97@gmail.com>
2020-11-01 15:49:48 +0600

    integrated graphql and apollo with react and koa with shopify polaris

---
+++ pages/components/ProductList.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import gql from 'graphql-tag';
+import { useQuery } from '@apollo/react-hooks';
+import store from 'store-js';
+import { Card, ResourceList, Stack, TextStyle, Thumbnail } from '@shopify/polaris';
+
+const GET_PRODUCTS_BY_ID = gql`
+	query getProducts($ids: [ID!]!) {
+		nodes(ids: $ids) {
+			... on Product {
+				title
+				handle
+				id
+				images(first: 1) {
+					edges {
+						node {
+							originalSrc
+							altText
+						}
+					}
+				}
+				variants(first: 1) {
+					edges {
+						node {
+							price
+							id
+						}
+					}
+				}
+			}
+		}
+	}
+`;
+
+const ProductList = () => {
+	const { loading, error, data } = useQuery(GET_PRODUCTS_BY_ID, {
+		variables: { ids: store.get('ids') },
+	});
+
+	if (loading) {
+		return <div>Loading Products...</div>;
+	} else if (error) {
+		return <div>{error.message}</div>;
+	}
+	console.log('ProductList -> data', data);
+
+	return (
+		<div>
+			<h1>Product List</h1>
+		</div>
+	);
+};
+
+export default ProductList;
---
+++ pages/index.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import store from 'store-js';
+import { ResourcePicker } from '@shopify/app-bridge-react';
+import { EmptyState, Layout, Page } from '@shopify/polaris';
+
+import ProductList from './components/ProductList';
+
+const Index = () => {
+	const [modal, setModal] = useState({ open: false });
+
+	const emptyState = !store.get('ids');
+
+	const handleSelection = (resources) => {
+		const ids = resources.selection.map((product) => product.id);
+		setModal({ open: false });
+		store.set('ids', ids);
+		console.log('handleSelection -> store.ids', store.get('ids'));
+	};
+
+	return (
+		<Page>
+			<ResourcePicker
+				resourceType='Product'
+				showVariants={false}
+				open={modal.open}
+				onCancel={() => setModal({ open: false })}
+				onSelection={(resources) => handleSelection(resources)}
+			></ResourcePicker>
+			<Layout>
+				{emptyState ? (
+					<EmptyState
+						heading='Manage your inventory transfers'
+						action={{
+							content: 'Select Products',
+							onAction: () => setModal({ open: true }),
+						}}
+						image='https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg'
+					>
+						<p>Select Products</p>
+					</EmptyState>
+				) : (
+					<ProductList></ProductList>
+				)}
+			</Layout>
+		</Page>
+	);
+};
+
+export default Index;
