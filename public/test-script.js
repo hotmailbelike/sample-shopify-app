f2e40b89bacd69f96fdf9ebd6d3294c78d264d2a
hotmailbelike <mdabdullahomar97@gmail.com>
2020-11-01 18:04:19 +0600

    need to fix test-script as there is jquery code in it

---
+++ public/test-script.js
@@ -0,0 +1,16 @@
+console.log('this is from script tag api!!!!');
+
+// const header = $('header.site-header').parent();
+
+// header
+// 	.prepend(
+// 		`
+//   <div>
+//     Hello this is coming from the public folder!!
+//   </div>
+// `
+// 	)
+// 	.css({
+// 		'background-color': 'orange',
+// 		'text-align': 'center',
+// 	});
